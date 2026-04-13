daeaeab349b0bde55b52558341c831b35d0f5d39
shashank2300 <43846651+shashank2300@users.noreply.github.com>
2026-04-13 23:32:12 +0530

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.go
@@ -0,0 +1,25 @@
+func coinChange(coins []int, amount int) int {
+    dp := make([]int, amount+1)
+    for i := range dp {
+        dp[i] = amount + 1
+    }
+    dp[0] = 0
+    for a := 1; a <= amount; a++ {
+        for _, c := range coins {
+            if a-c >= 0 {
+                dp[a] = min(dp[a], 1+dp[a-c])
+            }
+        }
+    }
+    if dp[amount] > amount {
+        return -1
+    }
+    return dp[amount]
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
