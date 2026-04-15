c954b4217ceb4f1eb1f362d9740f0d35e64a2d6e
shashank2300 <43846651+shashank2300@users.noreply.github.com>
2026-04-15 22:41:30 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.go
@@ -0,0 +1,21 @@
+func uniquePaths(m int, n int) int {
+    dp := make([][]int, m)
+
+	for i := range m {
+		dp[i] = make([]int, n)
+	}
+
+	dp[m-1][n-1] = 1
+	for i := m-1; i >= 0; i-- {
+		for j := n-1; j >= 0; j-- {
+			if i < m-1 {
+				dp[i][j] += dp[i+1][j]
+			}
+			if j < n-1 {
+				dp[i][j] += dp[i][j+1]
+			}
+		}
+	}
+
+	return dp[0][0]
+}
