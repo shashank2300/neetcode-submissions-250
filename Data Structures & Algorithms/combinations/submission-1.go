533ae031deb769b6795417ea68acb0536c676584
shashank2300 <43846651+shashank2300@users.noreply.github.com>
2026-05-14 07:22:05 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.go
@@ -0,0 +1,26 @@
+func combine(n int, k int) [][]int {
+    res := [][]int{}
+
+    var dfs func(int, []int)
+    dfs = func(i int, cur []int) {
+        if len(cur) == k {
+            temp := make([]int, k)
+            copy(temp, cur)
+            res = append(res, temp)
+            return
+        }
+
+		if i > n {
+			return
+		}
+
+        for j := i; j <= n; j++ {
+            cur = append(cur, j)
+            dfs(j+1, cur)
+            cur = cur[:len(cur)-1]
+        }
+    }
+
+    dfs(1, []int{})
+    return res
+}
