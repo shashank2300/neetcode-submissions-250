ee3f4a6b40748b97b15797817eae536ace6dd39a
shashank2300 <43846651+shashank2300@users.noreply.github.com>
2026-05-21 00:24:19 +0530

    Add: jump-game-vii - submission-0

---
+++ Data Structures & Algorithms/jump-game-vii/submission-0.go
@@ -0,0 +1,24 @@
+func canReach(s string, minJump int, maxJump int) bool {
+    n := len(s)
+    if s[n-1] == '1' {
+        return false
+    }
+
+    dp := make([]bool, n)
+    dp[0] = true
+    cnt := 0
+
+    for i := 1; i < n; i++ {
+        if i >= minJump && dp[i-minJump] {
+            cnt++
+        }
+        if i > maxJump && dp[i-maxJump-1] {
+            cnt--
+        }
+        if cnt > 0 && s[i] == '0' {
+            dp[i] = true
+        }
+    }
+
+    return dp[n-1]
+}
