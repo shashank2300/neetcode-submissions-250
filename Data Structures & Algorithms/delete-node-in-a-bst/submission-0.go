c142b4061c930f57c5d37915df3181ccf2a6f4d5
shashank2300 <43846651+shashank2300@users.noreply.github.com>
2026-06-20 22:47:15 +0530

    Add: delete-node-in-a-bst - submission-0

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-0.go
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ 
+func deleteNode(root *TreeNode, key int) *TreeNode {
+    if root == nil {
+        return nil
+    }
+
+    if key > root.Val {
+        root.Right = deleteNode(root.Right, key)
+    } else if key < root.Val {
+        root.Left = deleteNode(root.Left, key)
+    } else {
+        if root.Left == nil {
+            return root.Right
+        }
+        if root.Right == nil {
+            return root.Left
+        }
+
+        cur := root.Right
+        for cur.Left != nil {
+            cur = cur.Left
+        }
+        cur.Left = root.Left
+        return root.Right
+    }
+
+    return root
+}
