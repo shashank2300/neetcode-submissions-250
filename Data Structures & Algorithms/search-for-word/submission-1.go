f9b79eb33a93ecec9b93198012e770b66d202b21
shashank2300 <43846651+shashank2300@users.noreply.github.com>
2026-07-17 22:52:11 +0530

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.go
@@ -0,0 +1,34 @@
+func exist(board [][]byte, word string) bool {
+	rows, cols := len(board), len(board[0])
+
+	var dfs func(r, c, i int) bool
+	dfs = func(r, c, i int) bool {
+		if i == len(word) {
+			return true
+		}
+
+		if r < 0 || r >= rows || c < 0 || c >= cols || board[r][c] != word[i] || board[r][c] == '#' {
+			return false
+		}
+
+		temp := board[r][c]
+
+		board[r][c] = '#'
+		res  := dfs(r+1, c, i+1) ||
+				dfs(r-1, c, i+1) ||
+				dfs(r, c+1, i+1) ||
+				dfs(r, c-1, i+1)
+		board[r][c] = temp
+
+		return res
+	}
+
+	for r := 0; r< rows; r++ {
+		for c := 0; c < cols; c++ {
+			if dfs(r, c, 0) {
+				return true
+			}
+		}
+	}
+	return false
+}
